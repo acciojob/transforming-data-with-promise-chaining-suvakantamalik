06ef1197cdbee7f1a1827c61dd9fdcfbd6df70d5
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-23 13:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const outputBox = document.getElementById("output");
+function delayPromise(value, delay){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(value);
+		}, delay);
+	})
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+	const inputNumber = document.getElementById("ip").value;
+
+	delayPromise(inputNumber, 2000)
+		.then((number) => {
+			outputBox.innerText = `Result: ${number}`;
+			return delayPromise(number * 2, 1000);
+		})
+		.then((number) => {
+			outputBox.innerText = `Result: ${number}`;
+			return delayPromise(number - 3, 1000);
+		})
+		.then((number) => {
+			outputBox.innerText = `Result: ${number}`;
+			return delayPromise(number / 2, 1000);
+		})
+		.then((number) => {
+			outputBox.innerText = `Result: ${number}`;
+			return delayPromise(number + 10, 1000);
+		})
+		.then((number) => {
+			outputBox.innerText = `Result: ${number}`;
+			outputBox.innerText = `Final Result: ${number}`;
+		})
+		.catch((error) => {
+			outputBox.innerText = `Error: ${error.message}`;
+		})
+})
+
